9904eb7e3df75491653655084a90777171ecdd33
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2017-11-02 19:48:37 -0400

    wget now uses -N
    using -N to download by timestamp instead of old weird tagging method with -nc. Commented all old lines.

--- update.bat
+++ update.bat
@@ -15,17 +15,17 @@ set zipballtype=tagname
 :: ================================================================================
 
 :: we find the last version we downloaded
-echo Checking installed version . . .
-echo.
-for /F "delims=" %%a in (lasttag) do set "lasttag=%%a"
-if not exist "lasttag" ( set lasttag=none )
-echo.
-echo current version: %lasttag%
+REM echo Checking installed version . . .
+REM echo.
+REM for /F "delims=" %%a in (lasttag) do set "lasttag=%%a"
+REM if not exist "lasttag" ( set lasttag=none )
+REM echo.
+REM echo current version: %lasttag%
 
 
 REM we download header information from github api
 echo.
-echo Checking server for updates . . .
+REM echo Checking server for updates . . .
 "%~dp0bin\wget" -nv -nc --content-disposition https://api.github.com/repos/%user%/%repo%/releases/latest
 REM echo %errorlevel%
 if not %errorlevel%==0 (
@@ -35,20 +35,20 @@ if not %errorlevel%==0 (
 	exit
 )
 for /F "delims=" %%a in ('findstr /I "browser_download_url" latest') do set "link=%%a"
-for /F "delims=" %%a in ('findstr /I "tag_name" latest') do set "tag=%%a"
-set tag=%tag:~15,-2%
+REM for /F "delims=" %%a in ('findstr /I "tag_name" latest') do set "tag=%%a"
+REM set tag=%tag:~15,-2%
 del latest
 
 
-if %lasttag%==%tag% (
-	echo.
-	echo Already up to date [%lasttag%]
-	pause
-	exit
-) else (
-	echo.
-	echo Update found! [%lasttag% --^> %tag%]
-)
+REM if %lasttag%==%tag% (
+	REM echo.
+	REM echo Already up to date [%lasttag%]
+	REM pause
+	REM exit
+REM ) else (
+	REM echo.
+	REM echo Update found! [%lasttag% --^> %tag%]
+REM )
 
 
 if %type%==bin (
@@ -66,11 +66,11 @@ if %type%==bin (
 :: ====================
 if %zipballtype%==tagname (
 	REM echo https://github.com/%user%/%repo%/archive/%tag%.zip
-	"%~dp0bin\wget" -nc https://github.com/%user%/%repo%/archive/%tag%.zip
+	"%~dp0bin\wget" -N https://github.com/%user%/%repo%/archive/%tag%.zip
 	pause
 	goto :finished
 ) else if %zipballtype%==api (
-	"%~dp0bin\wget" -nc --content-disposition https://api.github.com/repos/%user%/%repo%/zipball
+	"%~dp0bin\wget" -N --content-disposition https://api.github.com/repos/%user%/%repo%/zipball
 	pause
 	goto :finished
 ) else (
@@ -85,7 +85,7 @@ REM @echo on
 :: ====================
 :: USE THIS PART IF THE RELEASE IS LIKE COMPILED OR SOMETHING
 :: ====================
-"%~dp0bin\wget" -nc %link:~30%
+"%~dp0bin\wget" -N %link:~30%
 pause
 goto :finished
 
@@ -104,6 +104,6 @@ echo Installing . . .
 REM PUT THE INSTALL COMMANDS HERE
 REM for /f %%f in ('dir /b /o-d "FILEFILEFILE"') do echo %%f
 REM bin\7z x -o "%installdir%" -y "%%~ff"
-echo %tag% >lasttag
+REM echo %tag% >lasttag
 pause
 exit
